aed60fca0fad2a6be5edeb0af1fa4fa03bf157ca
saravanan642 <saravanansenthil605@gmail.com>
2025-11-30 13:44:53 +0530

    updated Increment.js

--- src/components/publicpages/Increment.js
+++ src/components/publicpages/Increment.js
@@ -1,34 +1,30 @@
-
-
 import React, { useState } from 'react';
 
 function Increment() {
 
-  const [add, setAdd] = useState(200);  
+    const [add, setAdd] = useState(200);
+    const [state, setState] = useState("Shankareshwari");
+    const [color, setColor] = useState("bg-blue-500");
 
-  console.log("Default", add);
-
-  
-  const [state, setstate] = useState("Shankareshwari");
-  console.log ("Default ", state);
-
-  
+    return (
+        <div>
 
 
-  
-
-  return (
-    <div>
-      <button onClick={() => setAdd(add - 20)}> ADD button </button> 
-      <h2>Value: {add}</h2>
+            <button onClick={() => setAdd(add - 20)}> ADD button </button>
+            <h2>Value: {add}</h2>
 
 
-      <button onDoubleClick={()=>setstate("saravanan")} >Click mee</button>
-      <h2> {state}</h2>
-    </div>
+            <button onDoubleClick={() => setState("saravanan")}>Click mee</button>
+            <h2>{state}</h2>
 
-    
-  );
+
+            <div className={`${color} p-4`}>
+                 </div>
+            <button onClick={() => setColor("bg-red-600")}>clor</button>
+
+
+        </div>
+    );
 }
 
-export default Increment;  
+export default Increment;

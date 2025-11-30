6193a9dc4caae4b6edabc25c46468375dacda433
saravanan642 <saravanansenthil605@gmail.com>
2025-11-30 15:02:16 +0530

    updated Increment.js

--- src/components/publicpages/Increment.js
+++ src/components/publicpages/Increment.js
@@ -1,28 +1,32 @@
 import React, { useState } from 'react';
+import img1 from "../assests/pexels-nishantaneja-2362699.jpg";
+import img2 from "../assests/pexels-philippedonn-1133957.jpg";
 
 function Increment() {
-
     const [add, setAdd] = useState(200);
     const [state, setState] = useState("Shankareshwari");
     const [color, setColor] = useState("bg-blue-500");
+    
+    
+    const [image, setImage] = useState(img1);
 
     return (
         <div>
 
-
             <button onClick={() => setAdd(add - 20)}> ADD button </button>
             <h2>Value: {add}</h2>
-
 
             <button onDoubleClick={() => setState("saravanan")}>Click mee</button>
             <h2>{state}</h2>
 
+            <div className={`${color} p-4`}></div>
+            <button onClick={() => setColor("bg-red-600")}>Color</button>
 
-            <div className={`${color} p-4`}>
-                 </div>
-            <button onClick={() => setColor("bg-red-600")}>clor</button>
+            
+            <button onClick={() => setImage(img2)}>Change Image</button>
 
-
+            
+            <img src={image} alt="Selected" width="300" />
         </div>
     );
 }

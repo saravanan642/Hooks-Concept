0945107f8e919ba636ff5fa8fea6a092fe533d25
saravanan642 <saravanansenthil605@gmail.com>
2025-12-10 12:53:14 +0530

    upadte Home.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Header } from './components/Layout.js/Header'
 import { Footer } from './components/Layout.js/Footer'
 import Increment from './components/publicpages/Increment'
 import Sample from './UseState/Sample'
+import Home from './UseState/Home'
 
 
 
@@ -11,6 +12,7 @@ export const App = () => {
   return (
     <div>
       <Header />
+      <Home />
        <RoutePages />
        <Footer/>
        <Increment/>
---
+++ src/UseState/Home.js
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+  return (
+    <div>
+      <button className='border border-solid  bg-red-600 p-[20px] m-[30px]'>Click mee</button>
+    </div>
+  )
+}
+
+export default Home
